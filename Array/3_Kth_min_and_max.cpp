b005fa5520d75bb9f08281b05eba6e9ca63ba481
Ayush Gupta <721ayush@gmail.com>
2020-11-01 12:19:33 +0530

    kth element min and max

---
+++ Array/3_Kth_min_and_max.cpp
@@ -0,0 +1,24 @@
+/*
+ *   Copyright (c) 2020 
+ *   All rights reserved.
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int k;
+    cin >> k;
+
+    sort(arr, arr + n);
+    cout << arr[k - 1];
+    return 0;
+}
