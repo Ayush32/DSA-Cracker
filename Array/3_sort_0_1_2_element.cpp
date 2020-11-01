6b4c969921f20d2587a9bd7748dc6bd4384a37d5
Ayush Gupta <721ayush@gmail.com>
2020-11-01 13:00:37 +0530

    sorting the array

---
+++ Array/3_sort_0_1_2_element.cpp
@@ -0,0 +1,24 @@
+/*
+ *   Copyright (c) 2020 
+ *   All rights reserved.
+ */
+#include <iostream>
+using namespace std;
+
+void sortArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+}
