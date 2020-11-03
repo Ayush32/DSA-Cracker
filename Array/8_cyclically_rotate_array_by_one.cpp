7fb32fd526c02ce961efa0c76ea94f3b171e2f15
Ayush Gupta <721ayush@gmail.com>
2020-11-03 11:47:56 +0530

    cyclic array

---
+++ Array/8_cyclically_rotate_array_by_one.cpp
@@ -0,0 +1,18 @@
+/*
+ *   Copyright (c) 2020 
+ *   All rights reserved.
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr1[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr1[i];
+    }
+}
