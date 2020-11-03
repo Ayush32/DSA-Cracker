af7f91b51fdded8900933b10428ee4d894f9338d
Ayush Gupta <721ayush@gmail.com>
2020-11-03 16:59:52 +0530

    arrays

--- Array/8_cyclically_rotate_array_by_one.cpp
+++ Array/8_cyclically_rotate_array_by_one.cpp
@@ -5,14 +5,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void rotate(int arr[], int m)
+{
+    int x = arr[m - 1], i;
+    for (i = m - 1; i >= 0; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = x;
+}
+
 int main()
 {
     int n;
     cin >> n;
-    int arr1[n];
+    int arr[n];
 
     for (int i = 0; i < n; i++)
     {
-        cin >> arr1[i];
+        cin >> arr[i];
     }
+    int m = sizeof(arr) / sizeof(arr[0]);
+
+    rotate(arr, m);
+    for (int j = 0; j < m; j++)
+    {
+        cout << arr[j] << " ";
+    }
+    return 0;
 }
