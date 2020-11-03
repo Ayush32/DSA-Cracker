c9ed70784a6af644c24c695aca78afb571c94091
Ayush Gupta <721ayush@gmail.com>
2020-11-03 20:25:18 +0530

    merge

---
+++ Array/13_mergerTwoSortArray.cpp
@@ -0,0 +1,22 @@
+/*
+ *   Copyright (c) 2020 
+ *   All rights reserved.
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int arr1[n], arr2[m];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr1[i];
+    }
+    for (int j = 0; j < m; j++)
+    {
+        cin >> arr2[j];
+    }
+}
