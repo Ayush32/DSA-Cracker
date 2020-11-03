4e06e1cb98473451c8a89426d3ff020e1c72dd86
Ayush Gupta <721ayush@gmail.com>
2020-11-03 22:50:25 +0530

    merge sort array

--- Array/13_mergerTwoSortArray.cpp
+++ Array/13_mergerTwoSortArray.cpp
@@ -2,8 +2,29 @@
  *   Copyright (c) 2020 
  *   All rights reserved.
  */
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+void mergeArray(int arr1[], int arr2[], int n, int m)
+{
+    int i = n - 1;
+    int j = 0;
+    while (i >= 0 && j < m)
+    {
+        if (arr1[i] >= arr2[j])
+        {
+            swap(arr1[i], arr2[j]);
+        }
+        else
+        {
+            break;
+        }
+        i--;
+        j++;
+    }
+    sort(arr1, arr1 + n);
+    sort(arr2, arr2 + m);
+}
 
 int main()
 {
@@ -19,4 +40,15 @@ int main()
     {
         cin >> arr2[j];
     }
+
+    mergeArray(arr1, arr2, n, m);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr1[i] << " ";
+    }
+    for (int j = 0; j < m; j++)
+    {
+        cout << arr2[j] << " ";
+    }
+    return 0;
 }
