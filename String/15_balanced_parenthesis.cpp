75bd21f0f3a679d2353ffabad03f0fc2ed1ba1f6
Ayush Gupta <721ayush@gmail.com>
2020-11-07 21:31:46 +0530

    balance

--- String/15_balanced_parenthesis.cpp
+++ String/15_balanced_parenthesis.cpp
@@ -5,8 +5,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool balanceParanthesis(string s){
+    stack<char>stk;
+
+    for(char c : s){
+        if(c == '(' || c == '{' || c == '['){
+            stk.push(c);
+        }
+        else{
+            if(stk.empty()){
+                return false;
+            }
+            else if(c == ')' && stk.top() != '('){
+                return false;
+            }
+            else if(c == '}' && stk.top() != '{'){
+                return false;
+            }
+            else if(c == ']' && stk.top() != '['){
+                return false;
+            }
+            stk.pop();
+        }
+
+    }
+    return stk.empty();
+}
+
 
 
 int main() {
-    
+    char str[100];
+    cin.getline(str,100);
+    if(balanceParanthesis(str)){
+        cout << "Balanced";
+    }
+    else{
+        cout << "Not Balanced";
+    }
+    return 0;
 }
