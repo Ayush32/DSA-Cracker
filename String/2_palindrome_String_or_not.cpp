0739a25513515592a77a144f37e80cc54e522a93
Ayush Gupta <721ayush@gmail.com>
2020-11-07 09:54:48 +0530

    palindrome string

--- String/2_palindrome_String_or_not.cpp
+++ String/2_palindrome_String_or_not.cpp
@@ -17,9 +17,39 @@ bool palindromeString(string s){
     }
 }
 
+// 2nd method
+
+bool palindromeStringMethod2(string s){
+        int start = 0;
+        int end = s.length()-1;
+        int i = 0;
+        int flag = 0;
+        while(start <= end){
+            if(s[start++] != s[end--]){
+                return false;
+            }
+        
+        }
+        return true;
+        
+}
+
 int main() {
     string s;
     cin >> s;
 
-    if(palind)
+    if(palindromeString(s)){
+        cout << "1";
+    }
+    else{
+        cout << "0";
+    }
+    cout<<endl;
+    if(palindromeStringMethod2(s)){
+        cout << "1";
+    }
+    else{
+        cout << "0";
+    }
+    return 0;
 }
