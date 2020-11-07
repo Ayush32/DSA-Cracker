f4cdb7710954c486b4ba66d785663d2a25b7f0a6
Ayush Gupta <721ayush@gmail.com>
2020-11-07 09:45:49 +0530

    string palindrome

---
+++ String/2_palindrome_String_or_not.cpp
@@ -0,0 +1,25 @@
+/*
+ *   Copyright (c) 2020 
+ *   All rights reserved.
+ */
+#include<bits/stdc++.h>
+using namespace std;
+
+bool palindromeString(string s){
+    string s1 = s;
+    reverse(s1.begin(),s1.end());
+
+    if(s1==s){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int main() {
+    string s;
+    cin >> s;
+
+    if(palind)
+}
