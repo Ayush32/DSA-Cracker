75ffaf210db760d14d06fd923b1dc468842a90cd
Ayush Gupta <721ayush@gmail.com>
2020-11-15 11:06:48 +0530

    set matrix zeroes

--- Day 1(Arrays)/6_MergeIntervasl.cpp
+++ Day 1(Arrays)/6_MergeIntervasl.cpp
@@ -34,6 +34,6 @@ vector<vector<int>> merge(vector<vector<int>> &intervals)
 int main()
 {
     vector<vector<int>> intervals = {{1, 3}, {2, 6}, {8, 10}, {8, 9}, {9, 11}, {15, 18}, {2, 4}, {16, 17}};
-    cout << merge(intervals);
+    merge(intervals);
     return 0;
 }
---
+++ Day 1(Arrays)/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+int main()
+{
+    vector<vector<int>> intervals = {{1, 3}, {2, 6}, {8, 10}, {8, 9}, {9, 11}, {15, 18}, {2, 4}, {16, 17}};
+    cout << merge(intervals);
+    return 0;
+}
